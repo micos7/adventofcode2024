d41849973c87b1c3cb8a030a11d855dd44304718
Mihai <micos7@gmail.com>
2025-05-27 15:10:10 +0300

    day 16 part 2

--- Day16/main.go
+++ Day16/main.go
@@ -9,7 +9,6 @@ import (
 )
 
 func main() {
-	// Use the actual filename for your input
 	fileName := "input.txt"
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -41,8 +40,7 @@ func main() {
 	}
 
 	simulatePart1Dijkstra(grid)
-
-	// printGrid(grid)
+	simulatePart2Dijkstra(grid)
 }
 
 type Point struct {
@@ -58,7 +56,6 @@ const (
 	DirRight Direction = 3
 )
 
-// Mapping from Direction to dr, dc
 var dirVectors = map[Direction]Point{
 	DirUp:    {-1, 0},
 	DirDown:  {1, 0},
@@ -79,20 +76,16 @@ type Node struct {
 type PriorityQueue []*Node
 
 func (pq PriorityQueue) Len() int { return len(pq) }
-
 func (pq PriorityQueue) Less(i, j int) bool {
 	return pq[i].Cost < pq[j].Cost
 }
-
 func (pq PriorityQueue) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
 }
-
 func (pq *PriorityQueue) Push(x interface{}) {
 	item := x.(*Node)
 	*pq = append(*pq, item)
 }
-
 func (pq *PriorityQueue) Pop() interface{} {
 	old := *pq
 	n := len(old)
@@ -104,8 +97,6 @@ func (pq *PriorityQueue) Pop() interface{} {
 func simulatePart1Dijkstra(grid [][]rune) int {
 	rows := len(grid)
 	cols := len(grid[0])
-
-	// Find the starting point 'S'
 	var start Point
 	foundS := false
 	for r := 0; r < rows; r++ {
@@ -120,7 +111,6 @@ func simulatePart1Dijkstra(grid [][]rune) int {
 			break
 		}
 	}
-
 	if !foundS {
 		return -1
 	}
@@ -139,13 +129,10 @@ func simulatePart1Dijkstra(grid [][]rune) int {
 
 	pq := make(PriorityQueue, 0)
 	heap.Init(&pq)
-
 	initialState := State{Point: start, Dir: DirRight}
 	initialNode := &Node{State: initialState, Cost: 0}
-
 	heap.Push(&pq, initialNode)
 	minCost[start.r][start.c][DirRight] = 0
-
 	minOverallCost := infinity
 
 	for pq.Len() > 0 {
@@ -154,32 +141,24 @@ func simulatePart1Dijkstra(grid [][]rune) int {
 		currentPoint := currentState.Point
 		currentDir := currentState.Dir
 		currentCost := currentNode.Cost
-
 		if currentCost > minCost[currentPoint.r][currentPoint.c][currentDir] {
 			continue
 		}
-
 		if grid[currentPoint.r][currentPoint.c] == 'E' {
 			if currentCost < minOverallCost {
 				minOverallCost = currentCost
 			}
 		}
-
 		for _, nextDir := range []Direction{DirUp, DirDown, DirLeft, DirRight} {
 			dr, dc := dirVectors[nextDir].r, dirVectors[nextDir].c
-
 			newR := currentPoint.r + dr
 			newC := currentPoint.c + dc
-
 			if newR >= 0 && newR < rows && newC >= 0 && newC < cols && grid[newR][newC] != '#' {
 				newCost := currentCost + 1
-
 				if nextDir != currentDir {
 					newCost += 1000
 				}
-
 				newState := State{Point: Point{newR, newC}, Dir: nextDir}
-
 				if newCost < minCost[newState.Point.r][newState.Point.c][newState.Dir] {
 					minCost[newState.Point.r][newState.Point.c][newState.Dir] = newCost
 					heap.Push(&pq, &Node{State: newState, Cost: newCost})
@@ -197,6 +176,150 @@ func simulatePart1Dijkstra(grid [][]rune) int {
 	}
 }
 
+func simulatePart2Dijkstra(grid [][]rune) int {
+	rows := len(grid)
+	cols := len(grid[0])
+	var start, end Point
+	foundS, foundE := false, false
+	for r := 0; r < rows; r++ {
+		for c := 0; c < cols; c++ {
+			if grid[r][c] == 'S' {
+				start = Point{r, c}
+				foundS = true
+			} else if grid[r][c] == 'E' {
+				end = Point{r, c}
+				foundE = true
+			}
+		}
+	}
+	if !foundS || !foundE {
+		return -1
+	}
+
+	const INF = 1000000000
+	dist := make([][][]int, rows)
+	for r := range dist {
+		dist[r] = make([][]int, cols)
+		for c := range dist[r] {
+			dist[r][c] = make([]int, 4)
+			for d := range dist[r][c] {
+				dist[r][c][d] = INF
+			}
+		}
+	}
+
+	pq := &PriorityQueue{}
+	heap.Init(pq)
+	startState := State{Point: start, Dir: DirRight}
+	dist[start.r][start.c][DirRight] = 0
+	heap.Push(pq, &Node{State: startState, Cost: 0})
+	minEndCost := INF
+
+	for pq.Len() > 0 {
+		current := heap.Pop(pq).(*Node)
+		state := current.State
+		cost := current.Cost
+		if cost > dist[state.Point.r][state.Point.c][state.Dir] {
+			continue
+		}
+		if state.Point.r == end.r && state.Point.c == end.c {
+			minEndCost = min(minEndCost, cost)
+			continue
+		}
+		dr, dc := dirVectors[state.Dir].r, dirVectors[state.Dir].c
+		nr := state.Point.r + dr
+		nc := state.Point.c + dc
+		if nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] != '#' {
+			newCost := cost + 1
+			if newCost < dist[nr][nc][state.Dir] {
+				dist[nr][nc][state.Dir] = newCost
+				newState := State{Point: Point{nr, nc}, Dir: state.Dir}
+				heap.Push(pq, &Node{State: newState, Cost: newCost})
+			}
+		}
+
+		var leftDir, rightDir Direction
+		switch state.Dir {
+		case DirUp:
+			leftDir, rightDir = DirLeft, DirRight
+		case DirDown:
+			leftDir, rightDir = DirRight, DirLeft
+		case DirLeft:
+			leftDir, rightDir = DirDown, DirUp
+		case DirRight:
+			leftDir, rightDir = DirUp, DirDown
+		}
+		for _, newDir := range []Direction{leftDir, rightDir} {
+			newCost := cost + 1000
+			if newCost < dist[state.Point.r][state.Point.c][newDir] {
+				dist[state.Point.r][state.Point.c][newDir] = newCost
+				newState := State{Point: state.Point, Dir: newDir}
+				heap.Push(pq, &Node{State: newState, Cost: newCost})
+			}
+		}
+	}
+
+	if minEndCost == INF {
+		return -1
+	}
+
+	onPath := make(map[Point]bool)
+	queue := []State{}
+	visited := make(map[State]bool)
+
+	for dir := Direction(0); dir < 4; dir++ {
+		if dist[end.r][end.c][dir] == minEndCost {
+			state := State{Point: end, Dir: dir}
+			queue = append(queue, state)
+			visited[state] = true
+			onPath[end] = true
+		}
+	}
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+		currentCost := dist[current.Point.r][current.Point.c][current.Dir]
+
+		dr, dc := dirVectors[current.Dir].r, dirVectors[current.Dir].c
+		prevR := current.Point.r - dr
+		prevC := current.Point.c - dc
+		if prevR >= 0 && prevR < rows && prevC >= 0 && prevC < cols &&
+			grid[prevR][prevC] != '#' && dist[prevR][prevC][current.Dir] == currentCost-1 {
+			prevState := State{Point: Point{prevR, prevC}, Dir: current.Dir}
+			if !visited[prevState] {
+				visited[prevState] = true
+				queue = append(queue, prevState)
+				onPath[Point{prevR, prevC}] = true
+			}
+		}
+
+		for prevDir := Direction(0); prevDir < 4; prevDir++ {
+			if prevDir == current.Dir {
+				continue
+			}
+			canTurn := false
+			switch prevDir {
+			case DirUp, DirDown:
+				canTurn = (current.Dir == DirLeft || current.Dir == DirRight)
+			case DirLeft, DirRight:
+				canTurn = (current.Dir == DirUp || current.Dir == DirDown)
+			}
+			if canTurn && dist[current.Point.r][current.Point.c][prevDir] == currentCost-1000 {
+				prevState := State{Point: current.Point, Dir: prevDir}
+				if !visited[prevState] {
+					visited[prevState] = true
+					queue = append(queue, prevState)
+					onPath[current.Point] = true
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Number of tiles on best paths: %d\n", len(onPath))
+	return len(onPath)
+}
+
 func printGrid(grid [][]rune) {
 	for _, row := range grid {
 		for _, cell := range row {
@@ -204,4 +327,11 @@ func printGrid(grid [][]rune) {
 		}
 		fmt.Println()
 	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
